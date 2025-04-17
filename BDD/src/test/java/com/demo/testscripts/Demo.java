2cda93f69ca900c73bcebb256801f33fe011aca9
Admin <Admin@DESKTOP-D1GL1U1>
2025-04-17 16:21:39 +0530

    added new package
    Demo and New Demo

---
+++ BDD/src/test/java/com/demo/testscripts/Demo.java
@@ -0,0 +1,17 @@
+package com.demo.testscripts;
+
+import org.testng.annotations.Test;
+
+public class Demo {
+
+	
+	@Test
+	public static void display() {
+
+		
+		System.out.println("Hello iam from Demo class");
+		System.out.println("Just try to push to github bro");
+		System.out.println("Maintainss jenkins code also");
+	}
+
+}
---
+++ BDD/src/test/java/com/demo/testscripts/NewDemo.java
@@ -0,0 +1,17 @@
+package com.demo.testscripts;
+
+import org.testng.annotations.Test;
+
+public class NewDemo {
+	
+	@Test
+	public void print() {
+	System.out.println("Hello iam from NewDemo class");
+	System.out.println("Just try to New push to github bro");
+	System.out.println("Maintainss New jenkins code also");
+
+}
+
+	
+
+}
